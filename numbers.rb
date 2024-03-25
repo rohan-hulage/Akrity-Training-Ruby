d4a88e3ade1f481de553ef06ed72995532d7e57f
Abhishek Hulage <abhihulage14@gmail.com>
2024-03-26 00:16:29 +0530

    New file numbers

---
+++ numbers.rb
@@ -0,0 +1,9 @@
+a = 5
+b = 6
+
+puts a + b
+
+# rand method : to generate random number
+10.times { puts rand(10)} # rand will will generate numbers from 1-9 it will exclude 10
+
+puts rand # if you dont pass any value to rand function it will print value from 0-1
